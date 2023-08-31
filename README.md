fba99b2344a7fb19e886e5e4941e88f05c1c3c8c
Adam Kaplan <adam.kaplan@redhat.com>
2023-08-31 11:08:57 -0400

    Initial Commit
    Initial set up of tkn-client-plugin using Jenkins Hello World tutorial
    template. Notable changes:
    
    - Use Apache 2.0 license instead of MIT

---
+++ README.md
@@ -0,0 +1,22 @@
+# `tkn` Client Plugin
+
+## Introduction
+
+A Jenkins plugin for running Tekton pipelines
+
+## Getting started
+
+TODO Tell users how to configure your plugin here, include screenshots, pipeline examples and 
+configuration-as-code examples.
+
+## Issues
+
+Report issues and enhancements on this GitHub project.
+
+## Contributing
+
+Refer to our [contribution guidelines](https://github.com/jenkinsci/.github/blob/master/CONTRIBUTING.md)
+
+## LICENSE
+
+Licensed under Apache 2.0, see [LICENSE](LICENSE.md)
---
+++ src/main/java/dev/tekton/plugins/jenkins/tkn/HelloWorldAction.java
@@ -0,0 +1,47 @@
+package dev.tekton.plugins.jenkins.tkn;
+
+import hudson.model.Run;
+import jenkins.model.RunAction2;
+
+public class HelloWorldAction implements RunAction2 {
+
+    private String name;
+    private transient Run<?, ?> run;
+
+    public HelloWorldAction(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Run<?, ?> getRun() {
+        return run;
+    }
+
+    @Override
+    public String getDisplayName() {
+        return "Greeting";
+    }
+
+    @Override
+    public String getIconFileName() {
+        return "document.png";
+    }
+
+    @Override
+    public String getUrlName() {
+        return "greeting";
+    }
+
+    @Override
+    public void onAttached(Run<?, ?> run) {
+        this.run = run;
+    }
+
+    @Override
+    public void onLoad(Run<?, ?> run) {
+        this.run = run;
+    }
+}
